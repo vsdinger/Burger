a157f5c306437a1ac41b88f0c0e260249228ed94
vsbakula <goldenrhesus2022@gmail.com>
2019-06-22 08:06:32 -0700

    npm, start of server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// required dependencies
+var express = require('express');
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+// Set MySQL
+var mysql = require("mysql")
+
+var PORT = process.env.PORT || 8080;
+
